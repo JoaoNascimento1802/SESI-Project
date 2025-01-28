6bd101d71cf696a3762b5bb7398ecd68380bd3fb
JoaoNascimento1802 <joao.emanuel.pn@gmail.com>
2025-01-28 09:39:02 -0300

    :wrench: chore: Estrutura inicial do Projeto

---
+++ src/test/java/com/example/sesi/SesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
